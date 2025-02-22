573bba8d4f57db183038bb702ba3628c6cbe5410
husseinbouik <husseinbouik5@gmail.com>
2025-02-22 19:17:06 +0100

    Refactor routing in App component and update imports; add Bootstrap CSS

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import TaskDashboard from "./pages/TaskDashboard";
-import Navbar from "./components/Navbar";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+import Dashboard from './pages/Dashboard';
+import Navbar from './components/layout/Navbar';
+import './App.css'; //Optional
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/tasks" element={<TaskDashboard />} />
-      </Routes>
+      <div className="container mt-3">  {/* Bootstrap container */}
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/" element={<Dashboard />} /> {/* Default route: dashboard */}
+        </Routes>
+      </div>
     </Router>
   );
 }
